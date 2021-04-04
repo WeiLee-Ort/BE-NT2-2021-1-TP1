2470d6f6245b5bdeb1ef01a2c54063a6ed3f8723
Wei <18900292@dev.azure.com>
2021-04-04 16:09:36 -0300

    Modificacion script y css

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 const classNames = {
-    TODO_ITEM: 'todo-container',
-    TODO_CHECKBOX: 'todo-checkbox',
-    TODO_TEXT: 'todo-text',
-    TODO_DELETE: 'todo-delete',
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+
+let cantTarea = 0
+let cantUncheck = 0
+let misTareas = []
+
+function addTodo() {
+  let tituloTarea = prompt('Que tarea quieres agregar?')
+  
+  if (tituloTarea != null){  
+    while (tituloTarea.trim() == ''){
+      alert('Debe ingresar algun texto')
+      tituloTarea = prompt('Que tarea quieres agregar?')
+    }     
+    if (tituloTarea != null){
+      
+      cantTarea++
+      itemCountSpan.innerHTML = cantTarea
+   
+      var li=document.createElement('li');
+      li.innerHTML = "<input type='checkbox' checked/>"
+      var text=document.createTextNode(tituloTarea);
+      li.appendChild(text)
+      list.appendChild(li);
+     
+      let checkBoxlist = document.querySelectorAll("ul input[type='checkbox']")        
+  
+      checkBoxlist[checkBoxlist.length-1].onclick = function() {
+         if(this.checked == false){
+            cantUncheck++ 
+            uncheckedCountSpan.innerHTML = cantUncheck
+            cantTarea--
+            itemCountSpan.innerHTML = cantTarea
+         }else{
+           cantUncheck--
+           uncheckedCountSpan.innerHTML = cantUncheck
+           // itemCountSpan.innerHTML = parseInt(itemCountSpan.innerHTML) + 1
+           cantTarea++
+           itemCountSpan.innerHTML = cantTarea
+         }             
+      }   
+    }
   }
-  
-  
-  const list = document.getElementById('todo-list')
-  const itemCountSpan = document.getElementById('item-count')
-  const uncheckedCountSpan = document.getElementById('unchecked-count')
-  
-  function addTodo() {
-    alert('Boton Add TODO clickeado!')
-  }
+}
